a2366a091f8274e21d0fdcd33a3c2c3909ffd224
Mohit Hooda <mhooda87@gmail.com>
2018-12-30 11:29:45 +0530

    Create 270A.cpp
    https://codeforces.com/problemset/problem/270/A

---
+++ 270A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    ll n;
+    cin>>n;
+    while(n--)
+          {
+              ll a;
+              cin>>a;
+              if(360%(180-a)==0)
+                cout<<"YES\n";
+    else cout<<"NO\n";
+          }
+}
+
