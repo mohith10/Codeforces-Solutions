2400ca305c6e5e34e728c2fefbf45732ad93432f
Mohit Hooda <mhooda87@gmail.com>
2018-12-30 18:59:52 +0530

    Create 507B.cpp
    https://codeforces.com/problemset/problem/507/B

---
+++ 507B.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef double db;
+int main()
+{
+    ll r,x,y,a,b;
+    cin>>r>>x>>y>>a>>b;
+    cout<<ceil(sqrt(pow(abs(x-a),2)+pow(abs(b-y),2))/(2*r));
+
+}
+
