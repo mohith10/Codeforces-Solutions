fbbd5191e2e5ef8bc48717245ccac893bfc9a3a0
Mohit Hooda <mhooda87@gmail.com>
2019-01-28 18:09:16 +0530

    Create 1107B.cpp

---
+++ 1107B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(
+{
+    ll n;
+    cin>>n;
+    while(n--)
+    {
+        ll k,x;
+        cin>>k>>x;
+        cout<<(k-1)*9+x<<endl;
+    }
+    
+}
