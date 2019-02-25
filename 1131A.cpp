b400f80e662cb5b509f40bc75b5a67b3efb756bb
Mohit Hooda <mhooda87@gmail.com>
2019-02-25 23:40:13 +0530

    Create 1131A.cpp

---
+++ 1131A.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void fast(){ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);}
+int main()
+{
+    fast();
+    ll w1,h1,w2,h2;
+    cin>>w1>>h1>>w2>>h2;
+    cout<<2*(w1+h1+h2)+4;
+}
