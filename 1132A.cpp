0c82a04f9659b05f3dd2ee884821eb1b4a69e2c2
Mohit Hooda <mhooda87@gmail.com>
2019-03-17 23:39:39 +0530

    Create 1132A.cpp

---
+++ 1132A.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+void fast(){ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);}
+int main()
+{
+    ll a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<(a==d && (c==0 || a!=0));
+}
